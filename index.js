31d72a5fae000330322fafc0cd24bf0f777d8221
Ivan Ariel Almendra <almendraivan210814@gmail.com>
2022-09-26 01:25:13 -0300

    agregando prueba

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const morgan = require('morgan')
+const moment = require('moment')
+const app = express()
+const fs = require('fs')
+
+const timeMoment =  moment().format('MMMM Do YYYY, h:mm:ss a');
+
+const loggerHTTP = fs.createWriteStream(__dirname + '/index.log' , {flags:'a'})
+
+app.use(morgan({stream:loggerHTTP}))
+
+const stringPrueba = '<-- Esta es una prueba a este horario'
+
+loggerHTTP.write( timeMoment + " |MSG| " + stringPrueba + "\n")
